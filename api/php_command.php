491c83006b660f0a060538e35d42c8ea9f2d12fe
Oubri Daouda <oubridaouda@gmail.com>
2023-09-15 13:04:12 +0000

    modify

--- api/php_command.php
+++ api/php_command.php
@@ -2,7 +2,7 @@
 <?php
 
 // Assurez-vous d'être dans le répertoire du projet Laravel
-$laravelRoot = __DIR__.'/';
+$laravelRoot = __DIR__ . 'php_command.php/';
 
 // Exécutez la commande "php artisan key:generate" en utilisant shell_exec
 $output = shell_exec("cd $laravelRoot && php artisan key:generate");
